a3b5477d6de676deb0112887ff8588ca377ee363
dvmfa90 <dvmfa90@gmail.com>
2023-04-26 22:45:16 +0100

    updated files

---
+++ NSG/new.ps1
@@ -0,0 +1,8 @@
+Write-Host '################################################' -fore yellow
+Write-Host 'EXPORT NSGs for Subscription into single file' -fore yellow
+Write-Host '################################################' -fore yellow
+# Connect to Azure
+Write-Host 'Connect to Azure'
+
+Connect-AzAccount
+
